ad1851384bd22ee579106bc5cc84c0914f52fcbe
daulen <daulenz@gmail.com>
2021-05-19 16:40:00 -0500

    commit tree bb508e10ee213ddd5ceda36558ec7f9a54876904

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,54 @@
+require 'pry'
+class Book
+    #.new alias
+    def initialize(title)
+        #instance variable attaches to eacj variable
+    #    binding.pry
+    #instance variable
+        @title =title
+        
+    end
+
+    #reader
+    def title
+        @title
+    end
+
+    #reader
+    def author
+        @author 
+    end
+
+    #reader
+    def page_count
+        @page_count
+    end
+
+    #writer
+    def author=(author_name)
+    @author = author_name
+    end
+
+    def page_count=(page_count)
+        @page_count =page_count
+    end
+
+    #writer
+
+    def genre=(genre)
+    @genre =genre
+    end
+
+    #eader
+    def genre
+        @genre
+    end
+
+    #instance method
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
